8eb32f03f561ebd1ec154b88292901f99fe2d46f
bethmij <bethmij@gmail.com>
2024-04-02 22:28:54 +0530

    set components

--- main.py
+++ main.py
@@ -21,10 +21,22 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=100, bg=YELLOW)
 
+label_timer = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40), bg=YELLOW)
+label_timer.grid(row=1, column=2)
+
 canvas = Canvas(width=210, height=230, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(103, 100, image=tomato)
 canvas.create_text(103, 125, text="00.00", font=(FONT_NAME, 30, "bold"), fill="white")
-canvas.pack()
+canvas.grid(row=2, column=2)
+
+button_start = Button(text="Start", bg="white", font=(FONT_NAME, 13, "bold"), highlightthickness=0)
+button_start.grid(row=3, column=1)
+
+button_reset = Button(text="Reset", bg="white", font=(FONT_NAME, 13, "bold"), highlightthickness=0)
+button_reset.grid(row=3, column=3)
+
+label_mark = Label(text="✔", font=(FONT_NAME, 20, "bold"), bg=YELLOW, fg=GREEN)
+label_mark.grid(row=4, column=2)
 
 window.mainloop()
