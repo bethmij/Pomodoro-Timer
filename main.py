cf46eca257948adb79cd78f8b71d7b98c112931f
bethmij <bethmij@gmail.com>
2024-04-03 00:02:58 +0530

    update start timer min n sec

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 2
-SHORT_BREAK_MIN = 1
-LONG_BREAK_MIN = 3
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 reps = 1
 
 
@@ -18,19 +18,21 @@ reps = 1
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global reps
-    work_rep = [1, 3, 5, 7]
-    short_break_rep = [2, 4, 6]
+    if reps == 8:
+        reps = 1
+    else:
+        reps += 1
 
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    if reps in work_rep:
-        count_down(work_sec)
-    elif reps in short_break_rep:
+    if reps % 2 == 0:
         count_down(short_break_sec)
-    elif reps == 1:
+    elif reps % 8 == 0:
         count_down(long_break_sec)
+    else:
+        count_down(work_sec)
 
 
 def restart_timer():
@@ -43,20 +45,14 @@ def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
-    if count == 0:
-        reps += 1
-
-        if reps == 9:
-            reps = 1
-
-        start_timer()
-
     if count_sec < 10:
         count_sec = f"0{count_sec}"
 
-    if count >= 0:
+    if count > 0:
         canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
